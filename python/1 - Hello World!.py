12c214c9d55932292a710779c69e0d6bf560f5b9
Mr Johnson Computing <42711962+mrjohnsoncomputing@users.noreply.github.com>
2020-07-16 14:22:23 +0100

    Create 1 - Hello World!.py

---
+++ python/1 - Hello World!.py
@@ -0,0 +1,25 @@
+# Hello! The Hash symbol at the start of the line tells the computer to ignore this.
+# We refer to these lines that the computer ignores as "Comments"
+# We can use comments in our code to explain what is going on, so that other people who look at our code can understand more easily. 
+# We might also use comments to make notes to ourselves
+
+
+# This is a string:
+# "Hello World"
+# A string is anything that is surrounded by "speech marks", or 'quote marks'.
+
+# my_string = "Hello World"
+# We have assigned this string to a variable called: my_string
+# We could have called it anything
+
+# This is another string, but the variable name doesn't really tell us much about what we should expect it to contain
+# cute_elephant = "Hello World"
+
+# We can get python to tell us what the value is of a variable by using the print() command
+# Inside the brackets, we put the value we want to be displayed to us
+# Type the print() command in the space below and in the brackets, enter Hello World as a string. 
+
+
+
+# It is much more common to use a variable, and print that instead
+# Do the same thing you did in the step above, but this time, put just the variable name in the brackets of the print() command.
