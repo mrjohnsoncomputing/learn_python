dcc3a5254c34dfe6b47371bebb04047975f0320a
Mr Johnson Computing <42711962+mrjohnsoncomputing@users.noreply.github.com>
2020-07-16 15:39:55 +0100

    Create 2 - Numbers.py

---
+++ python/2 - Numbers.py
@@ -0,0 +1,54 @@
+# Now that we have learnt how to print an output to the screen, let us do something useful with it.
+# We can use variables to store the result of a calculation like so:
+# result = 1 + 1
+# In the space below, create an appropriately named variable and set its value to the calculation 300 divided by 19
+# In programming the forward slash character "/" is used to represent divide, and the asterix character "*" is used to represent multiply.
+
+# v 2.1 - Code v #
+
+### ^^^^^^^^^^ ###
+
+# Now you can use the print() command to output the result of this calculation, remembering to put the variable name in the brackets of the print function.
+# Do this in the space below
+
+# v 2.2 - Code v #
+
+### ^^^^^^^^^^ ###
+
+# Numbers in programming can fall into many different categories, but keeping it simple we have Integer (int) and Floating Point (float).
+# An integer represents a whole number, and a floating point number represents any number with a decimal point, even things such as 10.0
+# The way a computer stores and performs calclations on an integer is different to that of a float, and so it is important we understand the difference.
+# What number type is the result from 2.2?
+# Type your answer as a string in the print() command in the space below.
+
+# v 2.3 - Code v #
+
+### ^^^^^^^^^^ ###
+
+# We can convert one type of number to another. Putting a float inside the brackets of int() will convert our float into an integer.
+# Try updating the code you wrote in 2.2 to have the answer print out as an integer rather than a decimal.
+# If you get stuck, read the next section for the answer. 
+
+# v 2.4 - Code v #
+
+### ^^^^^^^^^^ ###
+
+# Lets break down the code for the solution for the last section.
+# Your answer should have looked like this print(int(result)) [Your variable name might not be result, that's fine!]
+# The way the computer will execute this code will be to first take whatever is inside of the brackets of print and resolve it.
+# In order to resolve what is in the brackets, it performs the conversion from the command, int(result)
+# The result of this is then printed.
+# As a final activity, create an appropriately named variable and assign it to this calculation: 4 + 4 * 8
+# Print the variable so that you can see what the result is.
+# Before running the code, predict what the answer will be. 
+
+# v 2.5 - Code v #
+
+### ^^^^^^^^^^ ###
+
+# What mathemathical principle is the computer following?
+# How might you use brackets () in the calculation in order to get the result of 64 printing to the screen?
+
+# v 2.6 - Code v #
+
+### ^^^^^^^^^^ ###
