318753d7782e4ad9305e4b9768777d39f97fc655
Mr Johnson Computing <42711962+mrjohnsoncomputing@users.noreply.github.com>
2020-07-17 04:32:15 +0100

    Update 8 - Challenge.py

--- python/8 - Challenge.py
+++ python/8 - Challenge.py
@@ -1,5 +1,5 @@
-Title: 8 - Challenge.py
-Skills: Selection, Libraries, Output, Variables
+# Title: 8 - Challenge.py
+# Skills: Selection, Libraries, Output, Variables
 
 # You are now going to put to use what you have learnt so far.
 # Create a program which, every time it is run, it prints out a random name. There should be at least 5 names it can choose from.
