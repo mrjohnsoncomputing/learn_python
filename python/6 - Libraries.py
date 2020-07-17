dfc872732b69c921851d7095bba1d89dd8c1ca61
Mr Johnson Computing <42711962+mrjohnsoncomputing@users.noreply.github.com>
2020-07-17 04:03:15 +0100

    Create 6 - Libraries.py

---
+++ python/6 - Libraries.py
@@ -0,0 +1,12 @@
+# Title: 6 - Libraries.py
+# Skills: Modules, Variables, Strings, Integers, Math, Selection, Input & Output
+
+# A library, or module, is a file of code that you can load into your project in order to access extra commands which have been written by other people. 
+# Commands such as print(), input(), int() and str() as we have seen already come as standard in python, as they are common to almost all programming projects.
+# However, some commands are not always neccessary, and to save loading all of this code when you run your program...
+# ...these commands are kept separate for you to import as and when you need to.
+
+# Lets import our first library - the time library. 
+# To do this, we can use the line: import time
+# Add that in the space below
+
